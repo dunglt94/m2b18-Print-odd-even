27ff47bd75375e166d1dfc3c76b34ff19a2bbdbc
dunglt94 <tuandung9594@gmail.com>
2024-11-03 22:17:56 +0700

    first add

---
+++ src/EvenThread.java
@@ -0,0 +1,14 @@
+public class EvenThread extends Thread {
+    public void run() {
+        try {
+            for (int i = 1; i <= 10; i++) {
+                if (i % 2 == 0) {
+                    System.out.println("Even number: " + i);
+                }
+            }
+            Thread.sleep(15);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
---
+++ src/OddThread.java
@@ -0,0 +1,15 @@
+public class OddThread extends Thread {
+    @Override
+    public void run() {
+        try {
+            for (int i = 1; i <= 10; i++) {
+                if (i % 2 != 0) {
+                    System.out.println("Odd number: " + i);
+                }
+            }
+            Thread.sleep(10);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
